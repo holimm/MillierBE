c21c333057e430df8b08cf4c7f1a9b5a53ab1062
Nguyễn Lim Thái Hồ <kahn12345678@gmail.com>
2024-03-20 02:47:27 +0700

    Test

--- build/index.js
+++ build/index.js
@@ -1,83 +1,49 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = __importDefault(require("./server"));
-require("dotenv").config();
-var app = (0, server_1.default)();
-var _a = require("mongodb"), MongoClient = _a.MongoClient, ServerApiVersion = _a.ServerApiVersion;
-var uri = "mongodb+srv://kahn12345678:5PVr7u4VNFnIu8vq@milliler.z95myk3.mongodb.net/?retryWrites=true&w=majority";
-var client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
-app.get("/", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var err_1;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, client.connect()];
-            case 1:
-                _a.sent();
-                client
-                    .db("millilerDB")
-                    .collection("users")
-                    .find()
-                    .toArray(function (err, result) {
-                    if (err)
-                        throw err;
-                    res.send({ result: result });
-                    res.json({ msg: "Your data has been saved" });
-                    console.log("Get UIPath Email List Successfully!");
-                });
-                return [3 /*break*/, 3];
-            case 2:
-                err_1 = _a.sent();
-                res.send({ status: "fail", message: "Gửi yêu cầu thất bại!" });
-                console.log(err_1);
-                return [3 /*break*/, 3];
-            case 3: return [2 /*return*/];
-        }
+exports.collections = void 0;
+var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var database_service_1 = require("./services/database.service");
+var users_router_1 = require("./routes/users.router");
+var products_router_1 = require("./routes/products.router");
+var productsdetail_router_1 = require("./routes/productsdetail.router");
+var category_router_1 = require("./routes/category.router");
+var orders_router_1 = require("./routes/orders.router");
+var email_router_1 = require("./routes/email.router");
+var blogs_router_1 = require("./routes/blogs.router");
+var contacts_router_1 = require("./routes/contacts.router");
+var app = (0, express_1.default)();
+var cors = require("cors");
+(0, database_service_1.connectToDatabase)()
+    .then(function () {
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS");
+        next();
     });
-}); });
+    app.use("/api/users", users_router_1.usersRouter);
+    app.use("/api/orders", orders_router_1.ordersRouter);
+    app.use("/api/products", products_router_1.productsRouter);
+    app.use("/api/category", category_router_1.categoryRouter);
+    app.use("/api/productsDetail", productsdetail_router_1.productsDetailRouter);
+    app.use("/api/email", email_router_1.emailRouter);
+    app.use("/api/blogs", blogs_router_1.blogsRouter);
+    app.use("/api/contacts", contacts_router_1.contactsRouter);
+    app.use(body_parser_1.default.json());
+    app.use(cors());
+    app.use(express_1.default.static("public"));
+    app.use("/images", express_1.default.static("images"));
+    app.listen(process.env.PORT, function () {
+        console.log("Server started at http://localhost:".concat(process.env.PORT));
+    });
+    app.use(cors());
+})
+    .catch(function (error) {
+    console.error("Database connection failed", error);
+    process.exit();
+});
+exports.collections = {};
